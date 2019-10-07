6b42b949307073473125ef8093e976c795c91cad
Por Sovan <sovan.por@truemoney.com.kh>
2019-10-07 15:24:06 +0700

    simple node app with test

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('hello world'))
+
+//listen to port 3000 by default
+// app.listen(process.env.PORT || 3000)
+ 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+module.exports = app;
