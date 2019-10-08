0c9269975ce9856cc3dd45cad1ff3a4dbc6774e1
sovan <porsovann@gmail.com>
2019-10-08 11:25:32 +0700

    add deployment script

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (req, res) => res.send('hey world'))
 
 //listen to port 3000 by default
 // app.listen(process.env.PORT || 3000)
