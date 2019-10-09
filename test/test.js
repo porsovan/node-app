4951c9f6386c53af2ebc673a5e2dda1f19bfd781
sovan <porsovann@gmail.com>
2019-10-09 15:40:45 +0700

    update test case

--- test/test.js
+++ test/test.js
@@ -1,8 +1,18 @@
 var request = require('supertest');
 var app = require('../index.js');
+var should = require("should");
+
 describe('GET /', function() {
  it('respond with hey world', function(done) {
  //navigate to root and check the the response is "hello world"
-    request(app).get('/').expect('hey world', done);
+    request(app)
+    .get('/')
+    .expect('hey world')
+    .end(function(err,res){
+        // HTTP status should be 200
+        res.status.should.equal(200);
+        done();
+     });
+     
  });
 });
